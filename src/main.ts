d21c4bc23c9882a88c614996cc078e397e7162cd
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-05-28 07:40:30 -0300

    1.14	Jest vs SWC

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rua Joaquim Marcerlino", 725, "13186-642", "Hortola
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10)
-const item2 = new OrderItem("2", "Item 2", 15)
+const item1 = new OrderItem("i1", "Item 1", 100, "p1", 0);
+const item2 = new OrderItem("i2", "Item 2", 200, "p2", 2);
 const order = new Order("1", "124", [item1,item2]);
