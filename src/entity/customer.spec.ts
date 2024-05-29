8d38ac59c7f0c680f826c3a277bd99449c8ff3ea
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-05-29 10:32:28 -0300

    2.7	Testando regra de reward points

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -53,5 +53,14 @@ describe("Customer unit tests", () => {
             customer.activate();
             
         }).toThrow("Address is mandatory to activate a");
-    });       
+    });
+    
+    it("shoul add reward points", () => {
+        const customer = new Customer("1", "Customer 1");
+        expect(customer.rewardPoints).toBe(0);
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(10);
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(20);        
+    })
 });
