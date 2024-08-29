7bf3d4786372eb9a3a4d3bb5ae7df5e43c761b0e
Ronaldo Aparecido da Silva <ronaldoapsilva_@hotmail.com>
2024-08-29 08:05:12 -0300

    Use cases | Retornando erro em teste de unidade

--- src/usecase/customer/find/find.customer.unit.spec.ts
+++ src/usecase/customer/find/find.customer.unit.spec.ts
@@ -24,7 +24,7 @@ describe("Unit Test find customer use case", () => {
 
     const input = {
       id: "123",
-    }
+    };
 
     const output = {
       id: "123",
@@ -42,4 +42,23 @@ describe("Unit Test find customer use case", () => {
     expect(result).toEqual(output);
 
   });
+
+
+  it("should not find a customer", async () => {
+    const customerRepository = MockRepository();
+    customerRepository.find.mockImplementation(() => {
+      throw new Error("Customer not found");
+    })
+    const usecase = new FindCustomerUseCase(customerRepository);
+
+    const input = {
+      id: "123",
+    };
+
+    expect(() => {
+      return usecase.execute(input);
+    }).rejects.toThrow("Customer not found");
+
+  })
+
 });
